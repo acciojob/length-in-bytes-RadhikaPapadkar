3b2bfb5c1ab652790209f51c27e62a40e22e905e
RadhikaPapadkar <radhikapapadkar226@gmail.com>
2025-01-10 09:11:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const byteSize = (str) => {
-  // write your code here
+ /**
+ * Function to calculate the byte size of a string
+ * @param {string} str - The input string
+ * @returns {number} - The size of the string in bytes
+ */
+function byteSize(str) {
+  // Create a Blob object with the input string
+  const blob = new Blob([str]);
+  // Return the size of the Blob in bytes
+  return blob.size;
+}
+
+// Examples
+console.log(byteSize('hello world')); // Output: 11
+console.log(byteSize('안녕하세요'));   // Output: 15
+console.log(byteSize(''));            // Output: 0
+ // write your code here
 };
 
 // Do not change the code below
